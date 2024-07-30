a7ffbbc8d5382be3f1867811c5866f34732411c6
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-07-30 20:20:34 +0300

    renamed route files to their perspective names and added POST and GET all to sessions

--- app/api/sessions/sessions.ts
+++ app/api/sessions/sessions.ts
@@ -1,13 +1,17 @@
 import { NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export async function GET(request: Request) {
+export async function GET(req: NextRequest) {
   const sessions = await prisma.session.findMany();
   console.log(sessions);
   return Response.json({ sessions });
 }
 export async function POST(req: NextRequest) {
   const body = await req.json();
-
-  return new Response('OK');
+  const session = await prisma.session.create({
+    data: {
+      date: body.date,
+    },
+  });
+  return Response.json({ session });
 }
