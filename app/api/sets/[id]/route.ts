976dafc1399a90b09dbac22046f0e92935ec4aaa
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-07-30 14:53:28 +0300

    added PUT by id  to sets

--- app/api/sets/[id]/route.ts
+++ app/api/sets/[id]/route.ts
@@ -14,6 +14,28 @@ export async function GET(
   return Response.json({ sets });
 }
 
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { weight, duration, repetitions, session_id, exercise_id } =
+    await req.json();
+  const ID = params.id;
+  const updatedSet = await prisma.set.update({
+    where: {
+      id: ID,
+    },
+    data: {
+      weight,
+      duration,
+      repetitions,
+      session_id,
+      exercise_id,
+    },
+  });
+  return Response.json({ updatedSet });
+}
+
 export async function DELETE(
   req: NextRequest,
   { params }: { params: { id: string } }
