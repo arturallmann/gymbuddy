5b3d37a40d39673d5af9a1510d48f11810ea3b83
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-08-13 19:14:20 +0300

    finished installing capacitor and added android to capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.gymbuddy.app',
   appName: 'gymbuddy',
-  webDir: 'public'
+  webDir: 'out',
+  bundledWebRuntime: false,
 };
 
 export default config;
