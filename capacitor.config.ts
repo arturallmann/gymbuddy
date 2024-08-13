f2ea4b24de4a85788368d6730b50edd25194aa6c
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-08-13 18:35:50 +0300

    installed capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gymbuddy.app',
+  appName: 'gymbuddy',
+  webDir: 'public'
+};
+
+export default config;
