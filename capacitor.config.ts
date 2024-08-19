86953fc6251c912f7d106be02bfac8c41be46dd2
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-08-19 17:56:55 +0300

    get local server working for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'gymbuddy',
   webDir: 'out',
-  bundledWebRuntime: false,
+  // bundledWebRuntime: false,
+  server: {
+    hostname: '192.168.1.66:3000',
+    cleartext: true,
+  },
 };
 
 export default config;
