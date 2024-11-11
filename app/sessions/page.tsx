20b9bfb46e97a0d25e36c3709eb0e9fcd1dbe9d0
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-11-11 19:07:56 +0200

    added button for starting a day, added calendar

--- app/sessions/page.tsx
+++ app/sessions/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 export default function Exercises() {
   const startDay = async () => {
+    const currentDate = new Date().toISOString();
     try {
       const response = await fetch('/api/sessions', {
         method: 'POST',
-        body: JSON.stringify({ date: new Date().toISOString() }),
+        body: JSON.stringify({ date: currentDate }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -15,16 +16,22 @@ export default function Exercises() {
   };
   return (
     <div className="top-0 left-0 w-screen h-screen">
-      <div className="bg-sky-950 fixed top-0 left-0 h-20 w-screen">
-        <p>Andres räägi välja</p>
-        <p>prill raisk</p>
+      <div className="bg-sky-950 flex top-0 left-0 h-20 w-screen">
+        <input
+          type="date"
+          value={new Date().toISOString().substring(0, 10)}
+          className="bg-sky-950 flex place-content-center w-screen"
+        />
       </div>
-      <button
-        className="bg-gray fixed top-20 left-0 w-20 h-20"
-        onClick={startDay}
-      >
-        <p>+</p>
-      </button>
+      <div className="bg-sky-500/50 w-20 h-20 rounded place-content-center">
+        <p className="bg-gray flex">Add Day</p>
+        <button
+          className="bg-gray flex centered top-20 left-0 w-20 h-20"
+          onClick={startDay}
+        >
+          <p>+</p>
+        </button>
+      </div>
     </div>
   );
 }
