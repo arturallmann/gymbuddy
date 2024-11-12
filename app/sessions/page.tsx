237dd03cd7d89faa84da122b0def95f1b928d205
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-11-12 16:16:46 +0200

    "add day" creates new day if one doesn't exist

--- app/sessions/page.tsx
+++ app/sessions/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 export default function Exercises() {
-  const startDay = async () => {
+  const createNewDay = async () => {
     const currentDate = new Date().toISOString();
     try {
       const response = await fetch('/api/sessions', {
@@ -14,6 +14,26 @@ export default function Exercises() {
       console.log(error);
     }
   };
+  const CheckForCurrentDay = async () => {
+    const date: String = new Date().toISOString().substring(0, 10);
+    const response = await fetch('/api/sessions', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const dates = await response.json();
+    let exists: boolean = false;
+    for (let i = 0; i <= dates.sessions.length; i++) {
+      if (date === dates.sessions[i].date.substring(0, 10)) {
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) {
+      createNewDay();
+    }
+  };
   return (
     <div className="top-0 left-0 w-screen h-screen">
       <div className="bg-sky-950 flex top-0 left-0 h-20 w-screen">
@@ -23,11 +43,11 @@ export default function Exercises() {
           className="bg-sky-950 flex place-content-center w-screen"
         />
       </div>
-      <div className="bg-sky-500/50 w-20 h-20 rounded place-content-center">
+      <div className="bg-sky-500/50 w-20 h-20 rounded">
         <p className="bg-gray flex">Add Day</p>
         <button
           className="bg-gray flex centered top-20 left-0 w-20 h-20"
-          onClick={startDay}
+          onClick={CheckForCurrentDay}
         >
           <p>+</p>
         </button>
